7cc357dbab6771a1b15c2cffe798ed934b89e8f6
Veronika <72826720+marvelousveronika@users.noreply.github.com>
2022-06-24 00:58:26 +0300

    rd

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,21 +3,25 @@ import { useNavigate } from 'react-router-dom'
 
 const Wrapper = styled.article`
   border-radius: 0.3rem;
-  backgroung-color: ${props => props.theme.colorsUiBase};
+  background-color: ${props => props.theme.colorsUiBase};
   box-shadow: ${props => props.theme.shadow};
   cursor: pointer;
   overflow: hidden;
+  display: flex;
+  flex-direction: column;
 `
 
 const CardImage = styled.img`
   display:flex;
   width: 100%;
+  height: 50%;
   object-fit: cover;
   object-position: center;
   box-shadow: ${props => props.theme.shadow};
 `
 
 const CardBody = styled.div`
+  height: 50%;
   padding: 1rem 1.5rem 2rem;
   background-color: ${props => props.theme.colorsUiBase};
 `
